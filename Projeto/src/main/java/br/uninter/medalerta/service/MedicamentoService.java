9cbfc33f8c9aff7a5959c9b8438135e9b1790f41
monicagit-hub <githubmonica@gmail.com>
2026-04-20 06:51:46 -0300

    feat: adiciona MedicamentoService

---
+++ Projeto/src/main/java/br/uninter/medalerta/service/MedicamentoService.java
@@ -0,0 +1,34 @@
+package br.uninter.medalerta.service;
+
+import br.uninter.medalerta.model.Medicamento;
+import br.uninter.medalerta.repository.MedicamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedicamentoService {
+
+    @Autowired
+    private MedicamentoRepository repository;
+
+    public List<Medicamento> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Medicamento buscarPorId(Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Medicamento não encontrado! ID: " + id));
+    }
+
+    public Medicamento salvar(Medicamento medicamento) {
+        return repository.save(medicamento);
+    }
+
+    public void deletar(Integer id) {
+        buscarPorId(id);
+        repository.deleteById(id);
+    }
+
+}
